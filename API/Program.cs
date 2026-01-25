10d2c27514223e5f4d5c12d76c584543d08dc862
marshelmathews <marshelmathews65.com>
2026-01-25 17:18:39 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
@@ -13,7 +14,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 );
 
 var app = builder.Build();
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 // Configure the HTTP request pipeline.
 
 app.MapControllers();
